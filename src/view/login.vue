8dee99498b4e70cd0a602c1f5985412f058f1459
daisy <paipaicui@163.com>
2021-05-12 15:12:01 +0800

    获取list

--- src/view/login.vue
+++ src/view/login.vue
@@ -5,7 +5,17 @@
 </template>
 
 <script>
-export default {};
+import $ from 'axios';
+export default {
+  mounted() {
+    $({
+      methods: 'get',
+      url: 'admin/getList',
+    }).then((res) => {
+      console.log(res);
+    });
+  },
+};
 </script>
 
 <style>
